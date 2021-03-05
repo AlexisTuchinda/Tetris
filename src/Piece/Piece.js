0c6e348c81016a935af03c75b9f387c319644c5c
Alexis Tuchinda <50222834+AlexisTuchinda@users.noreply.github.com>
2021-03-04 19:03:33 -0800

    first push

---
+++ src/Piece/Piece.js
@@ -0,0 +1,10 @@
+import React from "react.js";
+
+class Piece extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            orientation = [] // 0 = empty, 1 = block
+        }
+    }
+}
