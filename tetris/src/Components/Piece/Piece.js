c919ca5e5c95d7a0c1b452ac586d09540df95856
Alexis Tuchinda <50222834+AlexisTuchinda@users.noreply.github.com>
2021-06-17 18:43:31 -0700

    Piece Rotation v.2

--- tetris/src/Components/Piece/Piece.js
+++ tetris/src/Components/Piece/Piece.js
@@ -19,9 +19,10 @@ class Piece{
     }
 
     make(){
+        this.parts =[];
         for (let y = 0; y < this.type.length; y++){
             for (let x = 0; x < this.type[y].length; x++){
-                if (this.type[y][x] == 1){
+                if (this.type[y][x] !=0){
                     this.parts.push(new Tile(8+x, y, this.ctx));
                 }
             }
@@ -30,22 +31,31 @@ class Piece{
 
     rotate(){
         if (constant.keys.space in constant.keysDown && this.falling){
-            
-            let [distX, distY] = [0, 0];
+            for (let y = 0; y < this.type.length; y++) {
+                for (let x = 0; x < y; x++) {
+                     [this.type[x][y], this.type[y][x]] = 
+                     [this.type[y][x], this.type[x][y]];
+                   }
+               }
+            this.type.reverse();
+            this.make();
 
-            this.parts.map((tile) =>{
-                distX+=tile.x;
-                distY+=tile.y;
+            // let [distX, distY] = [0, 0];
 
-                let x = -tile.x;
-                tile.x = tile.y;
-                tile.y = x; //however, doesn't stay in the relatively same place.
-            })
+            // this.parts.map((tile) =>{
+            //     distX+=tile.x;
+            //     distY+=tile.y;
 
-            for (let i = 0; i < this.parts.length; i++){
-                tile.x-=(Math.floor(distX/this.parts.length));
-            }
-            //MAKE PIECE STAY IN SAMME PLACE
+            //     let x = -tile.x;
+            //     tile.x = tile.y;
+            //     tile.y = x; //however, doesn't stay in the relatively same place.
+            // })
+
+            // this.parts.map((tile)=>{
+            //     console.log(tile.x);
+            //     tile.x-=(Math.floor(distX/this.parts.length));
+            //     console.log(tile.x);
+            // });
         }
     }
 
